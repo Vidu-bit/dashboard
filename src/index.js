eeed6812f3d8dcfaef24809183365889bcda9fc8
Vidu-bit <viduvtr@gmail.com>
2025-10-01 12:09:02 +0200

    Add first structure of lib

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/myChartLib.js
+export function createBarChart(canvasId, chartData, chartOptions = {}) {
+  const ctx = document.getElementById(canvasId).getContext('2d');
+  return new Chart(ctx, {
+    type: 'bar',
+    data: chartData,
+    options: {
+      responsive: true,
+      ...chartOptions
+    }
+  });
+}
